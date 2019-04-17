60d858f5bdc57aeab989597c7e3f7011f040f545
Viktor Dudal <vdudatc@training.local>
2019-04-17 18:59:20 +0300

    pre db security commit

--- src/main/java/ua/softserve/ita/config/RabotyNETInitializer.java
+++ src/main/java/ua/softserve/ita/config/RabotyNETInitializer.java
@@ -1,13 +1,6 @@
 package ua.softserve.ita.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
 
 public class RabotyNETInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
