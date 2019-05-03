14c364ae65c03b37ec009d1727f002cf10011c77
Den4ik18 <den.ohorodnik@gmail.com>
2019-05-03 17:27:23 +0300

    Some changes

--- src/main/java/ua/softserve/ita/model/Vacancy.java
+++ src/main/java/ua/softserve/ita/model/Vacancy.java
@@ -18,8 +18,9 @@ import java.util.Set;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+//@NamedQuery(name = Vacancy.FIND_BY_COMPANY, query = "select vac from Vacancy vac where vac.company_id = :id")
 public class Vacancy {
-
+    //public static final String FIND_BY_COMPANY = "Vacancy.findByCompany";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "vacancy_id")
