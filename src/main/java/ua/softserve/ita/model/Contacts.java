4b565a1dc81a91b3fc703d08ada7103a1036ebe2
OleksandrChornevich <Chornevich.A@gmail.com>
2019-05-03 18:22:50 +0300

    change

--- src/main/java/ua/softserve/ita/model/Contacts.java
+++ src/main/java/ua/softserve/ita/model/Contacts.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "contacts")
-public class Contacts implements Serializable {
+public class  Contacts implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
