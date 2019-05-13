5440ba430b2fb5e945d0de5be4901a54ffe9eb80
Tania <taniakytsun@gmail.com>
2019-05-13 18:02:17 +0300

    changed controller

--- src/main/java/ua/softserve/ita/controller/RegistrationController.java
+++ src/main/java/ua/softserve/ita/controller/RegistrationController.java
@@ -2,40 +2,55 @@ package ua.softserve.ita.controller;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.softserve.ita.dto.UserDto;
 import ua.softserve.ita.exception.ResourceNotFoundException;
 import ua.softserve.ita.model.User;
 import ua.softserve.ita.model.VerificationToken;
 import ua.softserve.ita.registration.OnRegistrationCompleteEvent;
-import ua.softserve.ita.service.letter.GenerateLetter;
 import ua.softserve.ita.service.UserService;
 import ua.softserve.ita.service.token.VerificationTokenService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
 public class RegistrationController {
 
-    private final GenerateLetter generateService;
+    private final VerificationTokenService verificationTokenService;
     private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
     private final VerificationTokenService tokenService;
 
-    public RegistrationController(GenerateLetter generateService, ApplicationEventPublisher eventPublisher, UserService userService, VerificationTokenService tokenService) {
-        this.generateService = generateService;
+    public RegistrationController(ApplicationEventPublisher eventPublisher, UserService userService, VerificationTokenService tokenService, VerificationTokenService verificationTokenService) {
         this.eventPublisher = eventPublisher;
         this.userService = userService;
         this.tokenService = tokenService;
+        this.verificationTokenService = verificationTokenService;
     }
 
     @GetMapping(value = "/user/{id}")
     public ResponseEntity<User> getPerson(@PathVariable("id") long id) {
         User user = userService.findById(id).orElseThrow(()-> new ResourceNotFoundException(String.format("User with id: %d not found", id)));
         return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping(value = "/users/{login}/")
+    public ResponseEntity<?> getByLogin(@PathVariable("login") String login) {
+//        if (!userService.findByEmail(login).isEmpty()){
+//            return ResponseEntity.ok(userService.findByEmail(login));
+//        }
+        List<User> users = userService.findByEmail(login);
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/users")
@@ -67,6 +82,31 @@ public class RegistrationController {
         User user = userService.createDTO(userDto);
         eventPublisher.publishEvent(new OnRegistrationCompleteEvent(user, getAppUrl(request)));
         return ResponseEntity.ok().body(user);
+    }
+
+    @RequestMapping(value = "/registrationConfirm", method = RequestMethod.GET)
+    public String confirmRegistration(final HttpServletRequest request, final Model model, @RequestParam("token") final String token) throws UnsupportedEncodingException {
+        Locale locale = request.getLocale();
+        final String result = verificationTokenService.validateVerificationToken(token);
+        if (result.equals("valid")) {
+            final Optional<User> user = userService.findByToken(token);
+
+            authWithoutPassword(user);
+            model.addAttribute("message", "account verified!");
+            return "redirect:/login";
+        }
+
+        model.addAttribute("message", "failed");
+        model.addAttribute("expired", "expired".equals(result));
+        model.addAttribute("token", token);
+        return "redirect:/badUser";
+    }
+
+    public void authWithoutPassword(Optional<User> user) {
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     private String getAppUrl(HttpServletRequest request) {
