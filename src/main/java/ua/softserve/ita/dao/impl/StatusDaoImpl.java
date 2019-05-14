bef5b943ff2786d2a44081aa195e732238360faf
Михайло Малярчук <michaelbodybuilder1999@gmail.com>
2019-05-14 15:43:53 +0300

    removed model status, added enum instead

--- src/main/java/ua/softserve/ita/dao/impl/StatusDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package ua.softserve.ita.dao.impl;
-
-import org.springframework.stereotype.Repository;
-import ua.softserve.ita.dao.StatusDao;
-import ua.softserve.ita.model.Status;
-
-@Repository
-public class StatusDaoImpl extends AbstractDao<Status,Long> implements StatusDao {
-}
--- src/main/java/ua/softserve/ita/dto/CompanyDTO/CompanyDTO.java
+++ src/main/java/ua/softserve/ita/dto/CompanyDTO/CompanyDTO.java
@@ -3,9 +3,9 @@ package ua.softserve.ita.dto.CompanyDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ua.softserve.ita.model.Status;
 import ua.softserve.ita.model.User;
 import ua.softserve.ita.model.Vacancy;
+import ua.softserve.ita.model.enumtype.Status;
 import ua.softserve.ita.model.profile.Address;
 import ua.softserve.ita.model.profile.Contact;
 
@@ -20,11 +20,11 @@ public class CompanyDTO {
     private String edrpou;
     private String description;
     private String website;
+    private Status status;
     private Contact contact;
     private Address address;
     private String logo;
     private Set<Vacancy> vacancies;
-    private Status status;
     private User user;
 
 }
