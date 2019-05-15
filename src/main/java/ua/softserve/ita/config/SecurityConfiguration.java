3731a80a00e3d93ec58b68e71025b16fcfa13b0d
OleksandrChornevich <Chornevich.A@gmail.com>
2019-05-15 14:36:18 +0300

    change properties

--- src/main/java/ua/softserve/ita/config/SecurityConfiguration.java
+++ src/main/java/ua/softserve/ita/config/SecurityConfiguration.java
@@ -32,12 +32,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/companies").access("hasRole('ROLE_COWNER') or hasRole('ROLE_ADMIN')")
                 .antMatchers("/searchCV").access("hasRole('ROLE_COWNER')")
                 .antMatchers("/","/vacancies/**" , "/loginUser", "/registration").permitAll()
+                .antMatchers("/","/pdf/**" , "/updatePDF", "/createPdf/**").permitAll()
                 .antMatchers("/v2/api-docs",
                         "/configuration/ui",
                         "/swagger-resources",
                         "/configuration/security",
                         "/swagger-ui.html",
                         "/webjars/**").permitAll()
+
                 .anyRequest().authenticated()
                 .and()
                 .logout().logoutSuccessUrl("/logout")
