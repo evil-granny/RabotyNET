93f2c324c05c7b37384cccfd773c929b27b2efa9
DeH4uK <denshmain@gmail.com>
2019-05-15 19:30:31 +0300

    Merged photo and profile

--- src/main/java/ua/softserve/ita/config/SecurityConfiguration.java
+++ src/main/java/ua/softserve/ita/config/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -23,23 +23,27 @@ public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic()
                 .and()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/admin", "/person/**").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/users").access("hasRole('ROLE_USER')")
+//                .antMatchers("/users").access("hasRole('ROLE_USER')")
+                .antMatchers("/createCV").access("hasRole('ROLE_USER') or hasRole('ROLE_COWNER')")
                 .antMatchers("/companies").access("hasRole('ROLE_COWNER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/people").access("hasRole('ROLE_USER') or hasRole('ROLE_COWNER')")
                 .antMatchers("/searchCV").access("hasRole('ROLE_COWNER')")
+                .antMatchers("/", "/vacancies", "/login", "/registrationConfirm/**", "/registration", "/users/**").permitAll()
                 .antMatchers("/", "/vacancies/**", "/loginUser", "/registration").permitAll()
+                .antMatchers("/", "/pdf/**", "/updatePDF", "/createPdf/**").permitAll()
                 .antMatchers("/v2/api-docs",
                         "/configuration/ui",
                         "/swagger-resources",
                         "/configuration/security",
                         "/swagger-ui.html",
                         "/webjars/**").permitAll()
+
                 .anyRequest().authenticated()
-                .anyRequest().permitAll()
                 .and()
-                .logout().logoutSuccessUrl("/logoutUser")
+                .logout().logoutSuccessUrl("/logout")
                 .and().csrf().disable();
     }
 
