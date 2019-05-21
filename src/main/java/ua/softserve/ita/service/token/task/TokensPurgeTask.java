2b091c3ea534f57f3318c2b4e5637c34233788e0
Tania <taniakytsun@gmail.com>
2019-05-21 19:38:18 +0300

    fixed

--- src/main/java/ua/softserve/ita/service/token/task/TokensPurgeTask.java
+++ src/main/java/ua/softserve/ita/service/token/task/TokensPurgeTask.java
@@ -1,13 +1,10 @@
 package ua.softserve.ita.service.token.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.softserve.ita.service.token.VerificationTokenService;
 
-import java.time.Instant;
-import java.util.Date;
 
 @Service
 @Transactional
@@ -22,8 +19,7 @@ public class TokensPurgeTask {
     @Scheduled(cron = "${purge.cron.cleanOldTokens}")
     public void purgeExpired() {
 
-        Date now = Date.from(Instant.now());
 
-        verificationTokenService.deleteAllExpiredSince(now);
+        verificationTokenService.deleteAllExpiredSince();
     }
 }
