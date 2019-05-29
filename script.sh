f927f40fd5b7842d79c112aa06bb6d474c64ccc1
vdudatc <L-Y'H0nO>
2019-05-29 13:45:24 +0300

    script all -1

--- script.sh
+++ script.sh
@@ -3,9 +3,7 @@
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/RabotyNET/vacancies)
 
 if [[ "$status_code" -ne 200 ]] ; then
-    echo "Site status changed to $status_code"
-    exit 1
+    exit -1
 else
-    echo "Site status changed to $status_code"
     exit -1
 fi
