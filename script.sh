8d3a25bdb3f2404c655b032d164a6fd246616132
vdudatc <L-Y'H0nO>
2019-05-29 13:37:00 +0300

    changed script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# This is a comment!
-echo Hello World	# This is a comment, too!
 
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/RabotyNET/vacancies)
 
 if [[ "$status_code" -ne 200 ]] ; then
-  echo "Site status changed to $status_code"
-exit 1
+    echo "Site status changed to $status_code"
+    exit 1
 else
-echo "Site status changed to $status_code"
-  exit 0
+    echo "Site status changed to $status_code"
+    exit -1
 fi
