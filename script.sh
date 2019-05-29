6a035622116ef0ce719bfc9340064e154abb4aef
vdudatc <L-Y'H0nO>
2019-05-29 17:06:34 +0300

    changed script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/healthCheck1)
+status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/healthCheck1)
 
-if [[ "$status_code" -ne 200 ]] ; then
-    exit -1
+if [ "$status_code" -ne 200 ] ; then
+    exit 1
 else
     exit 0
 fi
