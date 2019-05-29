96e2c1750d82d74dfb025cb4f6b62dd81f7ce276
vdudatc <L-Y'H0nO>
2019-05-29 10:37:39 +0300

    added junit in pom and login controller test

---
+++ src/test/java/ua/softserve/ita/controller/LoginControllerTest.java
@@ -0,0 +1,15 @@
+package ua.softserve.ita.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @Test
+    public void someTest() {
+        assertEquals("", "");
+//        fail();
+    }
+
+}
