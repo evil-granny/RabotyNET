8c6340e4a0c99f37f07366acc9a49e86ab3be492
vdudatc <L-Y'H0nO>
2019-05-30 11:35:09 +0300

    test healthCheck

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/healthCheck1)
+status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/RabotyNET/healthCheck)
 
 if [ "$status_code" -ne 200 ] ; then
     exit 1
