fb54afe00274b050baab824782f76ef1fa2a0267
vdudatc <L-Y'H0nO>
2019-05-30 13:48:46 +0300

    customize context path

---
+++ src/main/java/ua/softserve/ita/config/AppContainerCustomizer.java
@@ -0,0 +1,17 @@
+package ua.softserve.ita.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppContainerCustomizer implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+
+        container.setPort(8080);
+        container.setContextPath("/RabotyNET");
+
+    }
+}
