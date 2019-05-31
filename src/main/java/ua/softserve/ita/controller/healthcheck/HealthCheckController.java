3217cd12a6518c67fcbc033b6cd64fe1c4a7c8c5
vdudatc <L-Y'H0nO>
2019-05-31 10:14:09 +0300

    changed script

--- src/main/java/ua/softserve/ita/controller/healthcheck/HealthCheckController.java
+++ src/main/java/ua/softserve/ita/controller/healthcheck/HealthCheckController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
 
-    @GetMapping("/healthCheck1")
+    @GetMapping("/healthCheck")
     public String  userLoginPost() {
         return "OK";
     }
