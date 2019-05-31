e52b7fbecacbc2004272a54fbd54707e7f5255f6
vdudatc <L-Y'H0nO>
2019-05-31 09:44:04 +0300

    changed script

--- script.sh
+++ script.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-#status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/RabotyNE)
-#echo "$status_code"
-if [[ $(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/RabotyNET/companies) -ne 200 ]] ; then
-echo "not 200"
-    exit 1
-else
-echo "200"
+set -e
+set -x
+
+echo "Wait 60 seconds for Jenkins to boot"
+sleep 60
+
+STATUS_CODE=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8080/RabotyNET/healthCheck1)
+
+echo "Curling against the Jenkins server"
+echo "Should expect a 200 within 15 seconds"
+
+if [[ "$STATUS_CODE" == "200" ]] ; then
+    echo "Jenkins has come up and ready to use"
+    echo "Returned: $STATUS_CODE"
     exit 0
+else
+    echo "Returned: $STATUS_CODE"
+    echo "Jenkins did not return a correct status code."
+    exit 1
 fi
