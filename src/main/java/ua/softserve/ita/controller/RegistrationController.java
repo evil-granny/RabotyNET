c00a5331c6730dc3188a603540a28130d0e89752
Tania <taniakytsun@gmail.com>
2019-06-02 14:47:42 +0300

    deleted unused methods

--- src/main/java/ua/softserve/ita/controller/RegistrationController.java
+++ src/main/java/ua/softserve/ita/controller/RegistrationController.java
@@ -1,23 +1,18 @@
 package ua.softserve.ita.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import ua.softserve.ita.dto.UserDto;
 import ua.softserve.ita.exception.ResourceNotFoundException;
 import ua.softserve.ita.model.User;
-import ua.softserve.ita.model.profile.Person;
+import ua.softserve.ita.model.VerificationToken;
 import ua.softserve.ita.registration.OnRegistrationCompleteEvent;
 import ua.softserve.ita.registration.RegistrationListener;
-import ua.softserve.ita.service.PersonService;
 import ua.softserve.ita.service.UserService;
 import ua.softserve.ita.service.token.VerificationTokenService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,31 +21,13 @@ public class RegistrationController {
 
     private final VerificationTokenService verificationTokenService;
     private final UserService userService;
-    private final PersonService personService;
     private final RegistrationListener registrationListener;
-    private final VerificationTokenService tokenService;
 
-    public RegistrationController( UserService userService, PersonService personService,
-                                   VerificationTokenService tokenService, VerificationTokenService verificationTokenService,
-                                   RegistrationListener registrationListener) {
+    public RegistrationController(UserService userService, VerificationTokenService verificationTokenService,
+                                  RegistrationListener registrationListener) {
         this.userService = userService;
-        this.personService = personService;
-        this.tokenService = tokenService;
         this.verificationTokenService = verificationTokenService;
         this.registrationListener = registrationListener;
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<List<User>> getAll() {
-        List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getPerson(@PathVariable("id") long id) {
-        User user = userService.findById(id).orElseThrow(() ->
-                new ResourceNotFoundException(String.format("User with id: %d not found", id)));
-        return ResponseEntity.ok().body(user);
     }
 
     @GetMapping(value = "/username/{login}/")
@@ -68,23 +45,9 @@ public class RegistrationController {
         return ResponseEntity.ok().body("User has been updated successfully.");
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") long id) {
-        User user = userService.findById(id).orElseThrow(() ->
-                new ResourceNotFoundException(String.format("User with id: %d not found", id)));
-        tokenService.findByUser(user).ifPresent(tokenService::delete);
-        userService.deleteById(id);
-        return ResponseEntity.ok().body(user);
-    }
-
     @PostMapping("/auth")
     public ResponseEntity<User> insert(@RequestBody @Valid UserDto userDto, final HttpServletRequest request) {
         User user = userService.createDTO(userDto);
-
-        Person person = new Person();
-        person.setUserId(user.getUserId());
-        personService.save(person);
-
         registrationListener.onApplicationEvent(new OnRegistrationCompleteEvent(user, getAppUrl(request)));
         return ResponseEntity.ok().body(user);
     }
@@ -93,25 +56,16 @@ public class RegistrationController {
     public String confirmRegistration(@RequestParam("token") final String token) {
         final String result = verificationTokenService.validateVerificationToken(token);
         if (result.equals("valid")) {
-            userService.findByToken(token).ifPresent((t)-> {
-                authWithoutPassword(t.getUser());
-                verificationTokenService.delete(t);
-            });
+            verificationTokenService.findByToken(token).ifPresent(verificationTokenService::delete);
         }
         return result;
     }
 
     @GetMapping(value = "/findToken")
     public String findToken(@RequestParam("email") String email) {
-        User  user = new User();
-        if (userService.findByEmail(email).isPresent()){
-            user = userService.findByEmail(email).get();
-        }
-        String verificationToken = null;
-        if(verificationTokenService.findByUser(user).isPresent()){
-           verificationToken = verificationTokenService.findByUser(user).get().getToken();
-        }
-        return verificationTokenService.validateVerificationToken(verificationToken);
+        User user = userService.findByEmail(email).orElse(new User());
+        VerificationToken verificationToken = verificationTokenService.findByUser(user).orElse(new VerificationToken());
+        return verificationTokenService.validateVerificationToken(verificationToken.getToken());
     }
 
     @PostMapping(value = "/resendAuthToken")
@@ -119,9 +73,9 @@ public class RegistrationController {
             HttpServletRequest request, @RequestParam String email) {
 
         Optional<User> user = userService.findByEmail(email);
-        if(!user.isPresent()){
+        if (!user.isPresent()) {
             return ResponseEntity.badRequest().body("Email not found!");
-        }else {
+        } else {
             verificationTokenService.findByUser(user.get()).ifPresent(verificationTokenService::delete);
             registrationListener.onApplicationEvent(new OnRegistrationCompleteEvent(user.get(), getAppUrl(request)));
             return ResponseEntity.ok().body("Successfully!");
@@ -130,16 +84,12 @@ public class RegistrationController {
 
     @RequestMapping(value = "/enabled/{email}/", method = RequestMethod.GET)
     public ResponseEntity<?> enabledUser(@PathVariable("email") String email) {
-        if(userService.findByEmail(email).isPresent()){
+        if (userService.findByEmail(email).isPresent()) {
             User user = userService.findByEmail(email).get();
             return ResponseEntity.ok(user.isEnabled());
-        }else return ResponseEntity.ok().body("User not found!");
+        } else return ResponseEntity.ok().body("User not found!");
     }
 
-    private void authWithoutPassword(User user) {
-        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
 
     private String getAppUrl(HttpServletRequest request) {
         return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
