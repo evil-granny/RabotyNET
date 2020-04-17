aba4e0ae8b8a80e49411c5c3985fb9c7b5c6de54
evil-granny <den.ohorodnik@gmail.com>
2020-04-17 16:31:51 +0300

    Changed '' on "" for script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,12 @@
 echo "Wait 30 seconds for Jenkins to boot"
 sleep 30
 
-STATUS_CODE=$(curl -s -o /dev/null -I -w '%{http_code}' localhost:8080/RabotyNET/healthCheck)
+STATUS_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" localhost:8080/RabotyNET/healthCheck)
 
 echo "Curling against the Jenkins server"
 echo "Should expect a 200 within 15 seconds"
 
+# shellcheck disable=SC2039
 if [[ ${STATUS_CODE} -ne 200 ]] ; then
     echo "Jenkins has come up and ready to use"
     echo "Returned: $STATUS_CODE"
